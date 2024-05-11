b3e5c2077e88be44d85775a6f67967974647d612
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-11 17:47:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main() {
-    int A;
-    cout << "Enter the number: ";
+    int A,B;
+    cout << "Enter the first number: ";
     cin >> A;
-    for(int i = 1; i <= A; i++) {
-        if(A % i == 0) {
+    cout << "Enter the second number: ";
+    cin >> B;
+    int min_ab = min(A,B);
+    for(int i = 1; i <= min_ab; i++) {
+        if(A % i == 0 && B%i==0) {
             cout << i << endl;
         }
     }
