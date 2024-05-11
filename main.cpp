703015551ff3e33204c2557b5e7699f571c7031b
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-11 16:46:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {
-    int sum=0;
+    int sum = 0;
     int desatky, sotni, odynytsi;
     for (int i = 100; i < 1000; i++) {
         odynytsi = (i % 10);
         desatky = (i % 100) / 10;
         sotni = i / 100;
-        if (odynytsi == desatky || odynytsi == sotni || desatky == sotni) {
+        if (odynytsi != desatky && odynytsi != sotni && desatky != sotni) {
             sum++;
         }
     }
