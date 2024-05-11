486ad536c35f342eaa6f83799c4ea663ed995aac
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-11 17:32:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
 int main() {
-    int sum = 0;
-    int desatky, sotni, odynytsi;
-    for (int i = 100; i < 1000; i++) {
-        odynytsi = (i % 10);
-        desatky = (i % 100) / 10;
-        sotni = i / 100;
-        if (odynytsi != desatky && odynytsi != sotni && desatky != sotni) {
-            sum++;
+    int A;
+    cout << "Enter A: ";
+    cin >> A;
+
+    cout << "All corect dividers: " << endl;
+
+    for (int B = 1; B < A; ++B) {
+        if (A % (B * B) == 0 && A % (B * B * B) != 0) {
+            cout << B << " ";
         }
     }
-    cout << sum;
+
+    cout << endl;
 
     return 0;
 }
