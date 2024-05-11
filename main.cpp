70911caef7a11cc63250442584f5043d39a5cbee
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-11 17:44:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
     int A;
-    cout << "Enter an integer A: ";
+    cout << "Enter the number: ";
     cin >> A;
-
-    int sumOfDigits = 0;
-    int tempA = abs(A); 
-    for (; tempA != 0; tempA /= 10) {
-        sumOfDigits += tempA % 10; 
-    }
-
-    int cubeOfSum = 1;
-    for (int i = 0; i < 3; ++i) {
-        cubeOfSum *= sumOfDigits;
-    }
-
-    if (cubeOfSum == A * A) {
-        cout << "The cube of the sum of digits of the number equals the square of number A" << endl;
-    } else {
-        cout << "The cube of the sum of digits of the number don't equal the square of number A" << endl;
+    for(int i = 1; i <= A; i++) {
+        if(A % i == 0) {
+            cout << i << endl;
+        }
     }
 
     return 0;
