0e0b642bcfe2555879939f8d0d3bf068fe667938
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-11 17:22:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
 int main() {
-    int sum = 0;
-    int desatky, sotni, odynytsi;
-    for (int i = 100; i < 1000; i++) {
-        odynytsi = (i % 10);
-        desatky = (i % 100) / 10;
-        sotni = i / 100;
-        if (odynytsi != desatky && odynytsi != sotni && desatky != sotni) {
-            sum++;
+    int number;
+    cout << "Enter any integer: ";
+    cin >> number;
+
+    int numDigits = 0;
+    int tempNumber = number;
+    for (; tempNumber != 0; tempNumber /= 10) {
+        numDigits++;
+    }
+
+    int result = 0;
+    int multiplier = 1;
+
+    for (int i = 0; i < numDigits; ++i) {
+        int digit = number % 10; 
+        number /= 10; 
+
+        if (digit != 3 && digit != 6) {
+            result += digit * multiplier;
+            multiplier *= 10; 
         }
     }
-    cout << sum;
+
+    cout << "Result after removing digits 3 and 6: " << result << endl;
 
     return 0;
 }
